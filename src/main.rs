e96609cb3fedda75e6084025b117f6113ea8c9f2
Olle Wreede <olle@wreede.se>
2022-06-20 22:03:43 +0200

    Support top/left offset

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,17 @@ async fn main() {
     let mut options = gif::DecodeOptions::new();
     options.set_color_output(gif::ColorOutput::RGBA);
     let mut decoder = options.read_info(input).unwrap();
+
+    let orig_x = screen_width() / 2. - decoder.width() as f32 / 2.;
+    let orig_y = screen_height() / 2. - decoder.height() as f32 / 2.;
+
     while let Some(frame) = decoder.read_next_frame().unwrap() {
         clear_background(WHITE);
-
         let image_texture = Texture2D::from_rgba8(frame.width, frame.height, &frame.buffer);
         draw_texture_ex(
             image_texture,
-            screen_width() / 2. - image_texture.width() / 2.,
-            screen_height() / 2. - image_texture.height() / 2.,
+            orig_x + frame.left as f32,
+            orig_y + frame.top as f32,
             WHITE,
             DrawTextureParams::default(),
         );
